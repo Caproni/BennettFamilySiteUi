7db9c965110229312db1b7829b2a67d8e760e355
Edmund Bennett <Edmund.Bennett@ghd.com>
2022-06-20 21:01:33 +0100

    Declared alert service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from "@angular/router";
+import {AlertWindowComponent} from "./alert/alert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlertWindowComponent,
   ],
   imports: [
     RouterModule.forRoot([]),
