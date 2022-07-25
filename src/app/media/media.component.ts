826e5f92b98fa8dae2376acb4ff82bbced782065
Edmund Bennett <Edmund.Bennett@ghd.com>
2022-07-25 09:34:04 +0100

    Working media search

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -29,7 +29,57 @@ export class MediaComponent implements OnInit {
   }
 
   filterMedia() {
+    const searchTerms: string[] = [];
+    console.log("Hi")
+    // @ts-ignore
+    const groups = this.searchPhrase.matchAll(this.searchRegex);
+    let group = groups.next();
+    while (!group.done) {
+      for (let i = 1; i < group.value.length; i++) {
+        if (group.value[i] !== undefined) {
+          searchTerms.push(group.value[i].toLowerCase());
+        }
+      }
+      group = groups.next();
+    }
 
+    this.filteredMedia = this.media.filter(item => {
+
+      const included: boolean[] = [];
+
+      const title = item.title?.toLowerCase();
+      const director = item.director?.toLowerCase();
+      const year = item.release_year?.toString();
+
+      for (const searchTerm of searchTerms) {
+        const includedForThisTerm: boolean[] = [];
+
+        if (title) {
+          includedForThisTerm.push(title.includes(searchTerm));
+        }
+
+        if (director) {
+          includedForThisTerm.push(director.includes(searchTerm));
+        }
+
+        if (year) {
+          includedForThisTerm.push(year.includes(searchTerm));
+        }
+
+        included.push(includedForThisTerm.reduceRight(
+          (accumulator, currentValue) => {
+            return accumulator || currentValue;
+          },
+          false
+        ));
+      }
+      return included.reduceRight(
+        (accumulator, currentValue) => {
+          return accumulator && currentValue;
+        },
+        true
+      );
+    });
   }
 
 }
--- src/app/media/medium-detail/medium-detail.component.ts
+++ src/app/media/medium-detail/medium-detail.component.ts
@@ -12,7 +12,6 @@ export class MediumDetailComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
