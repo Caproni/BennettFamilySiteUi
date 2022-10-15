8f46e4891a9be3fcc284dd90a8db92cd4c93db93
Edmund Bennett <Edmund.Bennett@ghd.com>
2022-10-15 08:15:22 +0100

    Working map

--- src/app/mapping/mapping.component.ts
+++ src/app/mapping/mapping.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import 'leaflet-providers'
 import * as L from 'leaflet';
 
 import { MapboxKeyReadService } from 'src/app/_services/api/mapping/mapbox-key-read.service';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-mapping',
   templateUrl: './mapping.component.html',
-  styleUrls: ['./mapping.component.css']
+  styleUrls: ['./mapping.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('400ms ease-out',
+              style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('400ms ease-in',
+              style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ],
 })
 export class MappingComponent implements OnInit {
+
+  windowWidth!: number;
+  windowHeight!: number;
 
   private map!: L.Map;
 
@@ -21,6 +48,13 @@ export class MappingComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.onInit();
+  }
+
+  onInit() {
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
+
     this.mapboxKeyReadService.readMapboxKey().subscribe((b) => {
       this.loadedMapboxKey = b;
       this.mapboxKey = this.mapboxKeyReadService.getMapboxKey();
@@ -35,6 +69,12 @@ export class MappingComponent implements OnInit {
       zoom: 7,
     });
     L.tileLayer.provider('Stamen.Toner').addTo(this.map);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
   }
 
 }
